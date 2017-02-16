596d5823a4233a34eb860f5722f578ed30bcb339
FelipeHarlen <felipeharlen@hotmail.com>
2017-02-15 21:43:52 -0300

    Acrescentada a classe LivrosCadastrados.

---
+++ src/java/main/LivrosCadastrados.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author FelipeHarlen
+ */
+public class LivrosCadastrados {
+    private ArrayList<Livro> listaLivros;
+    
+    public LivrosCadastrados() { 
+        listaLivros = new ArrayList<Livro>();
+    }
+}
